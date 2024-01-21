7c49f6dbef80f0025823d74ee0fa66a044c4bf0f
Dorian <minimickey427@yahoo.com>
2024-01-21 13:05:43 +0200

    Initial Commit

---
+++ American currency meme project/main.cpp
@@ -0,0 +1,38 @@
+//
+//  main.cpp
+//  American currency meme project
+//
+//  Created by Scarlat  Dorian Andrei on 21.01.2024.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    
+    string n;
+    string i;
+    string m;
+    string b;
+    
+    cout << "What american thing do you wanna see?  ";
+    getline(cin, n);
+    if(n=="I want to see the currency of America"){
+        cout << "America has a virtual currency named crypto/bitcoin, 1 crypto is approximativly 20k   ";
+    }
+    getline(cin, i);
+    if(i=="I want to get 1 trillion crypto"){
+        cout << "To get 1 trillion crypto, press the y key to download crypto  ";
+        cout << "Download:";
+    }
+    getline(cin, m);
+    if(m=="y"){
+        cout << "Now you have 1 trillion crypto, a trojan is installed in your computer, to get rid of the virus please call the nearest indian call center   ";
+    }
+    
+    
+    
+    return 0;
+}
